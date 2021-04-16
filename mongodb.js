670de3728265e7d7aca56edaf729a3bc2d7236ea
amy <amyjojjm2001@gmail.com>
2021-04-16 01:37:55 -0700

    ObjectID

--- mongodb.js
+++ mongodb.js
@@ -3,25 +3,35 @@
 const mongodb = require('mongodb')
 
 
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
+// const ObjectID = mongodb.ObjectID 
 const assert = require("assert");
+
+const {MongoClient, ObjectID} = require('mongodb')
  
 const url = "mongodb://127.0.0.1:27017";
 const dbName = "task-manager";
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
  
 MongoClient.connect(url, { useNewUrlParser:true, useUnifiedTopology: true }, (err, client) => {
   assert.equal(null, err);
   console.log("Connected successfully to server");
  
   const db = client.db(dbName);
-//   db.collection('users').insertOne({
-//       name:'June',
-//       age: 20
-//   }, (err, result) => {
-//       if (err) {
-//           return console.log('unable to insert user')
-//       }
-//       console.log(result.ops)
+  db.collection('users').insertOne({
+      _id: id,
+      name:'Victor',
+      age: 31
+  }, (err, result) => {
+      if (err) {
+          return console.log('unable to insert user')
+      }
+      console.log(result.ops)
+    })
+
 
 
 //   db.collection('users').insertMany([
