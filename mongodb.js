d3892aafd49f964c4d30f112159a96753674d1a1
amy <amyjojjm2001@gmail.com>
2021-04-16 03:00:23 -0700

    update documents

--- mongodb.js
+++ mongodb.js
@@ -18,27 +18,16 @@ MongoClient.connect(url, { useNewUrlParser:true, useUnifiedTopology: true }, (er
  
   const db = client.db(dbName);
 
-//   db.collection('users').findOne({_id: ObjectID('6077ed317e77200514a6ae95')},(error, user) => {
-//         if (error) 
-//             return console.log('unable to fetch')
-
-//         console.log(user)
-//     })
-
-    // db.collection('users').find({age:20}).toArray((error, users) => {
-    //     console.log(users)
-    // })
-
-    // db.collection('users').find({age:20}).count((error, count) => {
-    //     console.log(count)
-    // })
-
-    db.collection('tasks').findOne({_id: ObjectID("6077f1d0bad700053666ce8f")},(error, tasks) => {
-        console.log(tasks)
-    })
-
-    db.collection('tasks').find({completed: false}).toArray((error, uncompleted) => {
-        console.log(uncompleted)
+    updatePromise = db.collection('users').updateOne({
+        _id: new ObjectID('6077ed317e77200514a6ae95')
+    }, {
+        $inc: {
+            age:5
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
 })
  
