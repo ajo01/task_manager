259f784eaaf486ab233bddf65604ec4ccd383adf
amy <amyjojjm2001@gmail.com>
2021-04-16 01:22:17 -0700

    Init commit

---
+++ mongodb.js
@@ -0,0 +1,63 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb')
+
+
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+ 
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "task-manager";
+ 
+MongoClient.connect(url, { useNewUrlParser:true, useUnifiedTopology: true }, (err, client) => {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+//   db.collection('users').insertOne({
+//       name:'June',
+//       age: 20
+//   }, (err, result) => {
+//       if (err) {
+//           return console.log('unable to insert user')
+//       }
+//       console.log(result.ops)
+
+
+//   db.collection('users').insertMany([
+//       {
+//         name:'Andrew',
+//         age: 27
+//       },
+//       {
+//         name:'May',
+//         age: 19
+//       }
+//   ], (error, result)=> {
+    // if (error) {
+    //     return console.log('Unable to insert users')
+    // }
+    // console.log(result.ops)
+//   })
+
+
+//     db.collection('tasks').insertMany([
+//         {
+//             description:'HW1',
+//             completed: true
+//         },
+//         {
+//             description:'PA2',
+//             completed: false
+//         }
+
+// ], (error, result)=> {
+//     if (error) {
+//         return console.log('Unable to insert users')
+//     }
+//     console.log(result.ops)
+// })
+
+})
+ 
+
